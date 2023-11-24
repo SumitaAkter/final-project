f415efde0225841d43424244feb3cb20b8736199
Sumita Akter <sumitaakter530@gmail.com>
2023-11-25 00:04:59 +0600

    commit the header,product,shop and etc

---
+++ ema-john/src/components/Shop/Shop.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+    useEffect( () =>{
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+
+    }, [])
+
+    useEffect( ()=>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    }, [])
+
+    const handleAddToCart = (product) => {
+        // cart.push(product);
+        const newCart = [...cart, product]
+        setCart(newCart);
+        addToDb(product.id)
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products. map(product => <Product
+                        key = {product.id}
+                        product = {product}
+                        handleAddToCart = { handleAddToCart }
+                    ></Product> )
+                }
+            </div>
+            <div className="card-container">
+                <Cart cart = {cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
